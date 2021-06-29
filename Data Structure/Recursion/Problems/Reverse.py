13083f2b2c085eff3be9c8b941cbc9c5c283dd33
xiaomaomi7 <richard_qi@live.com>
2021-06-29 18:23:06 +0100

    Recursion-Reverse

---
+++ Data Structure/Recursion/Problems/Reverse.py
@@ -0,0 +1,26 @@
+'''
+Author: Hongxiang Qi
+Date: 27/06/2021
+Description:
+Write a recursive function called reverse which accepts a string and
+returns a new string in reverse.
+
+Example:
+    reverse('python') # 'nohtyp'
+    reverse('appmillers') # 'srellimppa'
+'''
+
+'''
+Apporach:
+    1. Recursive case - the flow
+        reverse(s) =    
+
+
+    2. Base condition - the stopping criterion
+        n = 0, return 0
+        n = 1, return 1
+
+    3. Unintentional case - the constraint
+        fib(1.1) - error - RecursionError: maximum recursion depth exceeded in comparison
+        fib(-1) - error - RecursionError: maximum recursion depth exceeded in comparison
+'''
