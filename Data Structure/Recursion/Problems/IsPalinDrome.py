a5142c956cade64302d0d41d3b37d36e4330428f
Hongxiang Qi <richard_qi@live.com>
2021-06-30 23:49:32 +0100

    Recursion_Problem - IsPalinDrome

---
+++ Data Structure/Recursion/Problems/IsPalinDrome.py
@@ -0,0 +1,29 @@
+'''
+Author: Hongxiang Qi
+Date: 30/06/2021
+Description:
+Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome
+(reads same forward and backward).
+Otherwise it returns false
+
+Example:
+    isPalindrome('awesome') # false
+    isPalindrome('foobar') # false
+    isPalindrome('tacocat') # true
+    isPalindrome('amanaplanacanalpanama') # true
+    isPalindrome('amanaplanacanalpandemonium') # false
+
+'''
+
+'''
+Apporach:
+    1. Recursive case - the flow
+        isPalindrome(s) =  
+
+
+    2. Base condition - the stopping criterion
+        
+
+    3. Unintentional case - the constraint
+
+'''
